5b3ce2f11e41bbab460f2409c44aba4eed55387f
aquaktus <aquaktus@yahoo.co.uk>
2020-03-15 17:29:29 +0000

    I think i fixed it

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ eventlet.monkey_patch()
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_socketio import SocketIO, emit, send
 import tweepy
-app = Flask(__name__, static_folder="./tom_is_lost_unity/builds/tom_is_lost_build")
+app = Flask(__name__, static_folder="./builds/tom_is_lost_build")
 
 auth = tweepy.OAuthHandler("MIjdhsGgxXDf6x42njMNoUGcp", "diqNByAalW57PxaHzHVeTZXVhWlLwOwtVtaKxz8ooaTFWqfG61")
 auth.set_access_token("1237176395740196865-VJGXrPrbyWEHTSeHNQivI0VOD1zabo", "gZrIXk5Ijy7W3kjgpiVUeHaGELIk1QTcOMDGJPfVeXcXs")
